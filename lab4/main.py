8d44bc79af5fb9ee48cdf8bf1678225627c42c4c
syn4z <iatcosorin797@gmail.com>
2023-10-29 20:17:15 +0200

    refactor: add random inputs; display tables; change names

--- lab4/main.py
+++ lab4/main.py
@@ -1,40 +1,50 @@
-from data import expansion_table, s_boxes, p_table
+from data import expansionTable, sBoxes, permutationTable
+import random
 
 
-def des_round(Li_minus_1, Ri_minus_1, subkey):
+def generateRandomInput(length):
+    return [random.randint(0, 1) for _ in range(length)]
+
+
+def displayTable(table, tableName):
+    print(f"{tableName} Table:")
+    for i in range(0, len(table), 8):
+        row = table[i:i + 8]
+        formattedRow = " ".join(f"{item:2d}" for item in row)
+        print(formattedRow)
+
+
+def desRound(LiMinus1, RiMinus1, subkey):
     # Expand the 32-bit input to 48 bits
-    expanded_Ri_minus_1 = [Ri_minus_1[i - 1] for i in expansion_table]
+    expandedRiMinus1 = [RiMinus1[i - 1] for i in expansionTable]
 
     # XOR with the subkey
-    xor_result = [int(expanded_Ri_minus_1[i]) ^ int(subkey[i]) for i in range(48)]
+    xorResult = [int(expandedRiMinus1[i]) ^ int(subkey[i]) for i in range(48)]
 
     # Pass the result through S-boxes
-    s_box_output = []
+    sBoxOutput = []
     for i in range(8):
-        s_box_input = xor_result[i * 6:(i + 1) * 6]
-        row = int(str(s_box_input[0]) + str(s_box_input[5]), 2)
-        col = int(''.join(map(str, s_box_input[1:5])), 2)
-        s_box_value = s_boxes[i][row][col]
-        s_box_output.extend([int(bit) for bit in format(s_box_value, '04b')])
+        sBoxInput = xorResult[i * 6:(i + 1) * 6]
+        row = int(str(sBoxInput[0]) + str(sBoxInput[5]), 2)
+        col = int(''.join(map(str, sBoxInput[1:5])), 2)
+        sBoxValue = sBoxes[i][row][col]
+        sBoxOutput.extend([int(bit) for bit in format(sBoxValue, '04b')])
 
-    # Permute the result using P-table
-    permuted_result = [s_box_output[i - 1] for i in p_table]
+    # Permute the result using Permutation-table
+    permutedResult = [sBoxOutput[i - 1] for i in permutationTable]
 
     # XOR with Li_minus_1 to get Ri
-    Ri = [int(permuted_result[i]) ^ int(Li_minus_1[i]) for i in range(32)]
+    Ri = [int(permutedResult[i]) ^ int(LiMinus1[i]) for i in range(32)]
 
     return Ri
 
 
-Li_minus_1 = [1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1,
-              1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1]
+LiMinus1Input = generateRandomInput(32)
+RiMinus1Input = generateRandomInput(32)
+subkeyInput = generateRandomInput(48)
 
-Ri_minus_1 = [0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1,
-              0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1]
+displayTable(expansionTable, "Expansion")
+displayTable(permutationTable, "\nPermutation")
 
-subkey = [1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1,
-          0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0,
-          0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1]
-
-Ri = des_round(Li_minus_1, Ri_minus_1, subkey)
-print("Ri:", Ri)
+resultRi = desRound(LiMinus1Input, RiMinus1Input, subkeyInput)
+print(f"\nRi:\n{resultRi}")
